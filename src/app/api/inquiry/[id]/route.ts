2db436dfd950c28af8ab8bf8ff1098d8278012c0
Rohit Joshi <rohitjoshi@Rohits-MacBook-Air.local>
2025-06-23 14:24:37 +0530

    new changes

--- src/app/api/inquiry/[id]/route.ts
+++ src/app/api/inquiry/[id]/route.ts
@@ -5,45 +5,43 @@ import { z } from "zod";
 
 const updateSchema = z.object({
     status: z.enum(["PENDING", "IN_PROGRESS", "RESOLVED"]),
-  });
+});
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } } 
 ) {
-    const token = req.cookies.get("accessToken")?.value
+    const token = req.cookies.get("accessToken")?.value;
 
-    if(!token){
+    if (!token) {
         return NextResponse.json({
-            success : false,
-            message : "Unauthorized"
-        }, {status: 401})
+            success: false,
+            message: "Unauthorized"
+        }, { status: 401 });
     }
 
-    const user = await verifyJWT(token)
+    const user = await verifyJWT(token);
 
-    if(!user || user.role !== "ADMIN"){
+    if (!user || user.role !== "ADMIN") {
         return NextResponse.json({
-            success : false,
-            message : "Forbidden"
-        },
-       {status : 403}
-    )
+            success: false,
+            message: "Forbidden"
+        }, { status: 403 });
     }
 
-    const {status} = updateSchema.parse(await req.json())
+    const { status } = updateSchema.parse(await req.json());
 
-     const id = params.id.replace(/[{}]/g, "")
+    const id = context.params.id.replace(/[{}]/g, "");
 
-     const inquiry = await prisma.inquiry.update({
-        where : {id},
-        data : {status}
-    })
+    const inquiry = await prisma.inquiry.update({
+        where: { id },
+        data: { status }
+    });
 
     return NextResponse.json({
-        success : true,
-        message : "Inquiry updated successfully",
-        data : inquiry
-    }, {status : 200})
+        success: true,
+        message: "Inquiry updated successfully",
+        data: inquiry
+    }, { status: 200 });
 }
 
