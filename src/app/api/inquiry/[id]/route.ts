4cddb258a537d3966ed5d08dc8fc22a38df936a4
Rohit Joshi <rohitjoshi@Rohits-MacBook-Air.local>
2025-06-24 10:21:23 +0530

    bug fixes

--- src/app/api/inquiry/[id]/route.ts
+++ src/app/api/inquiry/[id]/route.ts
@@ -3,55 +3,59 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";    
 
-
 const updateSchema = z.object({
-    status: z.enum(["PENDING", "IN_PROGRESS", "RESOLVED"]),
-  });
+  status: z.enum(["PENDING", "IN_PROGRESS", "RESOLVED"]),
+});
 
+export const PUT = async (
+  req: NextRequest,
+  context: { params: { id: string } }
+) => {
+  try {
+    const token = req.cookies.get("accessToken")?.value;
 
-export const  PUT = async(req: NextRequest, { params } : { params :{ id : string}}) => {
-   try {
+    if (!token) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized" },
+        { status: 401 }
+      );
+    }
 
-     const token = req.cookies.get("accessToken")?.value
- 
-     if(!token){
-         return NextResponse.json({
-             success : false,
-             message : "Unauthorized"
-         }, {status: 401})
-     }
- 
-     const user = await verifyJWT(token)
- 
-     if(!user || user.role !== "ADMIN"){
-         return NextResponse.json({
-             success : false,
-             message : "Forbidden"
-         },
-        {status : 403}
-     )
-     }
- 
-     const {status} = updateSchema.parse(await req.json())
- 
-     const id = params.id.replace(/[{}]/g, "");
- 
-      const inquiry = await prisma.inquiry.update({
-         where : {id},
-         data : {status}
-     })
- 
-     return NextResponse.json({
-         success : true,
-         message : "Inquiry updated successfully",
-         data : inquiry
-     }, {status : 200})
-   } catch (error) {
-      console.error(error);
-      return NextResponse.json({
+    const user = await verifyJWT(token);
+
+    if (!user || user.role !== "ADMIN") {
+      return NextResponse.json(
+        { success: false, message: "Forbidden" },
+        { status: 403 }
+      );
+    }
+
+    const { status } = updateSchema.parse(await req.json());
+
+    const id = context.params.id.replace(/[{}]/g, "");
+
+    const inquiry = await prisma.inquiry.update({
+      where: { id },
+      data: { status },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Inquiry updated successfully",
+        data: inquiry,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
         success: false,
         message: "An error occurred",
-        error: error instanceof Error ? error.message : String(error)
-     }, { status: 500 });
-   }
-}
+        error: error instanceof Error ? error.message : String(error),
+      },
+      { status: 500 }
+    );
+  }
+};
